0842aa08cd821b71d117f764dc1892cb461df1af
Amzad hossain <amzad.rafi@northsouth.edu>
2023-01-20 12:52:42 +0600

    login, logout, signup route added
    -- new loging route is added in auth.py file for login , logout, signup

--- website/auth.py
+++ website/auth.py
@@ -3,3 +3,18 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__) 
 
+
+#create a route decorator 
+@auth.route('/login') 
+def login(): 
+    return 'Login' 
+
+
+@auth.route('/logout') 
+def logout(): 
+    return 'Logout' 
+
+
+@auth.route('/signup') 
+def signup(): 
+    return 'Signup' 
